acc88c7de07893e5edbfe09b8e2eef503d89ff4d
Andrei Alikimovich <aa@plus8.pro>
2024-11-03 22:03:13 -0800

    @render children() support in button.svelte

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,13 +1,15 @@
 /** @type { import('@storybook/svelte').Preview } */
+import '../src/global.css';
+
 const preview = {
-  parameters: {
-    controls: {
-      matchers: {
-       color: /(background|color)$/i,
-       date: /Date$/i,
-      },
-    },
-  },
+	parameters: {
+		controls: {
+			matchers: {
+				color: /(background|color)$/i,
+				date: /Date$/i
+			}
+		}
+	}
 };
 
 export default preview;
--- src/stories/Button.stories.svelte
+++ src/stories/Button.stories.svelte
@@ -1,25 +1,36 @@
 <script module>
-  import { defineMeta } from '@storybook/addon-svelte-csf';
-  import Button from './Button.svelte';
-  import { fn } from '@storybook/test';
+	import { defineMeta, setTemplate, type Args } from '@storybook/addon-svelte-csf';
+	import Button from './Button.svelte';
+	import { fn } from '@storybook/test';
 
-  // More on how to set up stories at: https://storybook.js.org/docs/writing-stories
-  const { Story } = defineMeta({
-    title: 'Example/Button',
-    component: Button,
-    tags: ['autodocs'],
-    argTypes: {
-      backgroundColor: { control: 'color' },
-      size: {
-        control: { type: 'select' },
-        options: ['small', 'medium', 'large'],
-      },
-    },
-    args: {
-      onClick: fn(),
-    }
-  });
+	// More on how to set up stories at: https://storybook.js.org/docs/writing-stories
+	const { Story } = defineMeta({
+		title: 'Example/Button',
+		component: Button,
+		tags: ['autodocs'],
+		argTypes: {
+			size: {
+				control: { type: 'select' },
+				options: ['small', 'medium', 'large']
+			}
+		},
+		args: {
+			label: 'Button',
+			primary: false,
+			size: 'medium',
+			onClick: fn(),
+			children: 'Hey hey!'
+		}
+	});
 </script>
+
+<script lang="ts">
+	setTemplate(template);
+</script>
+
+{#snippet template({ children, ...args }: Args<typeof Story>)}
+	<Button {...args}>{children}</Button>
+{/snippet}
 
 <!-- More on writing stories with args: https://storybook.js.org/docs/writing-stories/args -->
 <Story name="Primary" args={{ primary: true, label: 'Button' }} />
