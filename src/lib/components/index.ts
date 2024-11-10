ed6614a8d7eb7e9ce473f2998964df39977358ad
Andrei Alikimovich <aa@plus8.pro>
2024-11-10 14:57:37 -0800

    ++

--- src/lib/components/index.ts
+++ src/lib/components/index.ts
@@ -1,4 +1,5 @@
 export { default as Button } from './Button/index.svelte';
 export { default as Icon } from './Icon/index.svelte';
 export { default as IconButton } from './IconButton/index.svelte';
+export { default as TextInput } from './TextInput/index.svelte';
 
